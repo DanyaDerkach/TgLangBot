83907a03b3b91827d7ed51bc6574db99cd579a3f
Danya <danyderkach@gmail.com>
2025-06-22 16:20:06 +0300

    testing implemented

--- handlers.py
+++ handlers.py
@@ -1,13 +1,40 @@
 from aiogram import Router, F
 from aiogram.types import Message, CallbackQuery
 from aiogram.filters import CommandStart, Command
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+from functools import wraps
 
 import keyboards as kb
 
 router = Router()
 
+class QuizState(StatesGroup):
+    testing = State()
+    level = State()
+    quiz = State()
+
+def no_state():
+    def decorator(handler):
+        @wraps(handler)
+        async def wrapper(message: Message, state: FSMContext, *args, **kwargs):
+            current_state = await state.get_state()
+            if current_state:
+                await message.answer("🚫 Ця дія зараз не доступна.\nНапишіть /exit, щоб вийти.")
+                return
+            return await handler(message, state, *args, **kwargs)
+        return wrapper
+    return decorator
+
+@router.message(Command("exit"))
+async def cmd_start(message: Message, state: FSMContext, callback: CallbackQuery):
+    await state.set_state(None)
+    await callback.message.edit_reply_markup(reply_markup=None)
+    await message.answer(f"", reply_markup=kb.main)
+
 @router.message(CommandStart())
-async def cmd_start(message: Message):
+@no_state()
+async def cmd_start(message: Message, state: FSMContext):
     await message.answer(f"""
 Hello, {message.from_user.full_name}!
 
@@ -21,8 +48,9 @@ Hello, {message.from_user.full_name}!
 • /mode — вибрати режим гри або тесту
 • /help — довідка""", reply_markup=kb.main)
 
-@router.message(Command('help'))
-async def get_help(message: Message):
+@router.message(Command("help"))
+@no_state()
+async def get_help(message: Message, state: FSMContext):
     await message.answer("""
 Ось доступні команди:
 
@@ -32,11 +60,69 @@ async def get_help(message: Message):
 /quiz — розпочати тестування з відкритою відповіддю
 /help — довідка""")
 
-@router.message(F.text == '✏️ Тестування')
-async def start_test(message: Message):
-    (await message.answer("Виберіть режим перекладу:", reply_markup=kb.language))
+@router.message(F.text == "❔ Довідка")
+@no_state()
+async def get_help(message: Message, state: FSMContext):
+    await message.answer("""
+Ось доступні команди:
 
-@router.callback_query(F.data == 'engtoua')
-async def test_engtoua(callback: CallbackQuery):
-    for i in range(0, 9):
-        await callback.message.edit_text("/10\nЯк перекладається слово:", reply_markup=kb.language)
+/start — перезапускає роботу бота
+/level — вибрати рівень англійської мови
+/test — розпочати тестування з варіантами відповіді
+/quiz — розпочати тестування з відкритою відповіддю
+/help — довідка""")
+
+@router.message(F.text =='💪 Складність')
+@no_state()
+async def start_test(message: Message, state: FSMContext):
+    await state.set_state(QuizState.level)
+    await message.answer("Виберіть складність:", reply_markup=kb.level)
+
+@router.message(QuizState.level)
+async def level_chosen(message: Message, state: FSMContext):
+    text = message.text.strip()
+    valid = {"A1", "A2", "B1","B2", "C1", "C2"}
+    parts = text.split()
+    level = parts[-1] if len(parts) > 0 else ""
+    if level not in valid:
+        await message.answer("❌ Невідомий рівень.")
+        return
+    await state.update_data(level=level)
+    await message.answer(f"✅ Рівень {level} обрано.", reply_markup=kb.main)
+    await state.set_state(None)
+
+@router.message(F.text == '✏️ Тестування')
+@no_state()
+async def start_test(message: Message, state: FSMContext):
+    await message.answer("Виберіть режим перекладу:", reply_markup=kb.language)
+
+@router.callback_query(F.data.in_(["eng_to_ua", "ua_to_eng"]))
+async def select_mode(callback: CallbackQuery, state: FSMContext):
+
+    await state.set_state(QuizState.testing)
+    level = (await state.get_data()).get("level", "A1")
+    word, correct_answer, keyboard = kb.generate_question(level)
+    await state.update_data(correct=correct_answer)
+
+    await callback.message.edit_text(f"🔤 Як перекладається слово: {word}?", reply_markup=keyboard)
+
+@router.callback_query(F.data.startswith("answer_"))
+async def check_answer(callback: CallbackQuery, state: FSMContext):
+    selected = callback.data.split("_", 1)[1]
+    data = await state.get_data()
+    correct = data.get("correct")
+    points = 0
+    if selected == correct:
+        result = "✅ Правильно!\n\n"
+        points = points + 1
+    else:
+        result= f"❌ Неправильно. Правильна відповідь: {correct}\n\n"
+
+    level = (await state.get_data()).get("level", "A1")
+    word, correct_answer, keyboard = kb.generate_question(level)
+    await state.update_data(correct=correct_answer)
+    await callback.message.edit_text(f"{result}🔤 Як перекладається слово: {word}?", reply_markup=keyboard)
+
+#@router.message(F.text == '💬 Вікторина')
+#async def start_test(message: Message):
+#    await message.answer("Виберіть режим перекладу:", reply_markup=kb.language)
--- keyboards.py
+++ keyboards.py
@@ -1,4 +1,11 @@
 from aiogram.types import ReplyKeyboardMarkup, InlineKeyboardMarkup, KeyboardButton, InlineKeyboardButton
+import json
+import random
+
+def load_words_from_json(current_level="A1") -> list[dict]:
+    with open("dictionary.json", "r", encoding="utf-8") as file:
+        data = json.load(file)
+    return data.get(current_level, [])
 
 main = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text='✏️ Тестування'), KeyboardButton(text='💬 Вікторина')],
@@ -7,6 +14,47 @@ main = ReplyKeyboardMarkup(keyboard=[
     resize_keyboard=True,
     input_field_placeholder='Оберіть дію...')
 
+exit_test = ReplyKeyboardMarkup(keyboard=[
+    [KeyboardButton(text='Завершити тестування')]
+],
+    resize_keyboard=True,
+    input_field_placeholder='Тестування...')
+
+level = ReplyKeyboardMarkup(keyboard=[
+    [KeyboardButton(text='🟢 A1'), KeyboardButton(text='🟢 A2')],
+    [KeyboardButton(text='🟡 B1'), KeyboardButton(text='🟡 B2')],
+    [KeyboardButton(text='🟠 C1'), KeyboardButton(text='🟠 C2')]
+],
+    resize_keyboard=True,
+    input_field_placeholder='Оберіть складність...')
+
 language = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text='ENG→UA', callback_data='engtoua'), InlineKeyboardButton(text='UA→ENG', callback_data='uatoeng')]
+    [InlineKeyboardButton(text='🇬🇧 ENG→UA', callback_data='eng_to_ua'), InlineKeyboardButton(text='🇺🇦 UA→ENG', callback_data='ua_to_eng')]
 ])
+
+def get_words(words: list[dict]) -> tuple[str, str, list[str]]:
+    correct = random.choice(words)
+    other_options = [w["ua"] for w in words if w != correct]
+    random.shuffle(other_options)
+    incorrect = other_options[:3]
+
+    all_options = incorrect + [correct["ua"]]
+    random.shuffle(all_options)
+
+    question_word = correct["en"]
+    correct_answer = correct["ua"]
+
+    return question_word, correct_answer, all_options
+
+def build_answer_keyboard(options: list[str]) -> InlineKeyboardMarkup:
+    buttons = [
+        [InlineKeyboardButton(text=opt, callback_data=f"answer_{opt}")]
+        for opt in options
+    ]
+    return InlineKeyboardMarkup(inline_keyboard=buttons)
+
+def generate_question(current_level="A1"):
+    words = load_words_from_json(current_level)
+    question_word, correct_answer, options = get_words(words)
+    keyboard = build_answer_keyboard(options)
+    return question_word, correct_answer, keyboard
